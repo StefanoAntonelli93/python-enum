b5a80312089cdac4ce41968e86a5ab489fd071be
StefanoAntonelli93 <ste.antonelli@outlook.it>
2025-01-03 22:55:27 +0100

    crete enum3

---
+++ enum3.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class StatoSemaforo(Enum):
+    ROSSO = 1
+    VERDE = 2
+    GIALLO = 3
+
+
+print(StatoSemaforo.ROSSO == StatoSemaforo.ROSSO)
+print(StatoSemaforo.ROSSO == StatoSemaforo.VERDE)
+
+print(StatoSemaforo.ROSSO is StatoSemaforo.ROSSO)
+print(StatoSemaforo.ROSSO is StatoSemaforo.VERDE)
+
+print(StatoSemaforo.ROSSO == 1)
