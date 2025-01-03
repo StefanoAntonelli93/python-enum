193a604d870d3dcaad11717e1d358dd2a3f008a0
StefanoAntonelli93 <ste.antonelli@outlook.it>
2025-01-03 22:45:31 +0100

    fixed enum2

--- enum2.py
+++ enum2.py
@@ -8,3 +8,13 @@ class PuntoCardinale(Enum):
 # iterazione   f-string   binding   interpolazione
 for direzione in PuntoCardinale:
     print(f"Nome: {direzione.name}, Valore: {direzione.value}")
+
+
+# SCEGLI DI ANDARE A NORD
+
+direzione_giusta = PuntoCardinale.NORD
+
+if direzione_giusta == PuntoCardinale.NORD:
+    print("Stai andando a Nord")
+else:
+    print("Stai andando nella direzione sbagliata")
