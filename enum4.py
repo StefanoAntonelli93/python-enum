8847c948614ac32f437355b35e6b9a9ce29b655c
StefanoAntonelli93 <ste.antonelli@outlook.it>
2025-01-05 15:21:05 +0100

    add decoretor @property to enum4

--- enum4.py
+++ enum4.py
@@ -5,11 +5,20 @@ class Stagione(Enum):
     ESTATE = 2
     AUTUNNO = 3
     INVERNO = 4
-
+    # DECORATORE
+    @property
+    def fa_caldo(self):
+        return self in {Stagione.PRIMAVERA, Stagione.ESTATE}
+    
     def descrizione(self):
         return {
             Stagione.PRIMAVERA: "Il tempo si riscalda",
             Stagione.ESTATE: "Andiamo al mare",
             Stagione.AUTUNNO: "Cadono le foglie",
             Stagione.INVERNO: "L'inverno sta arrivando!",
-        } [self]
+        } [self] 
+    
+stagione_corrente = Stagione.INVERNO   
+print(stagione_corrente.descrizione())
+
+print(stagione_corrente.fa_caldo)
